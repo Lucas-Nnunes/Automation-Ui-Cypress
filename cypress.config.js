5ec79bb886286c3b5c2594045cd9391841b7a0a8
Lucas Oliveira <lucasoliveiracerrito@gmail.com>
2024-01-02 15:58:36 -0300

    Atividade Docker File

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,19 @@
 const { defineConfig } = require("cypress");
+const Mochawesome = require("mochawesome");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    reporter: 'mochawesome',
+    baseUrl: 'http://lojaebac.ebaconline.art.br/',
+
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: false,
+      json: true,
     },
-    baseUrl: 'http://lojaebac.ebaconline.art.br/'
   },
+  
+ 
+
 });
